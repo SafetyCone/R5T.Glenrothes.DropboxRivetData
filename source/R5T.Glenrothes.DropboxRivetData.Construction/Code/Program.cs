c7e607106723b10010771a283d054266ee1a5494
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:43:12 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Glenrothes.DropboxRivetData.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Glenrothes.DropboxRivetData.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
