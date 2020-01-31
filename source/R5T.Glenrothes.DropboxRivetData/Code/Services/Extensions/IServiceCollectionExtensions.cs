db42b2f4ce2061621fe2c06dc7e26f6f0e9b78fa
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:32:20 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Glenrothes.DropboxRivetData/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Glenrothes.DropboxRivetData/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Findhorn.DropboxRivet;
 using R5T.Glenrothes.Findhorn;
 using R5T.Lombardy.Standard;
@@ -12,16 +13,27 @@ namespace R5T.Glenrothes.DropboxRivetData
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="ITestingDataDirectoryPathProvider"/> service.
+        /// </summary>
         public static IServiceCollection AddTestingDataDirectoryPathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<ITestingDataDirectoryPathProvider, DataDirectoryTestingDataDirectoryPathProvider>()
-                .AddDataDirectoryPathProvider()
-                .AddTestingDataDiretoryNameConvention()
-                .AddStringlyTypedPathOperator()
+            services.AddDataDirectoryTestingDataDirectoryPathProvider(
+                services.AddDataDirectoryPathProviderAction(),
+                services.AddTestingDataDiretoryNameConventionAction(),
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ITestingDataDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryPathProvider> AddTestingDataDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryPathProvider>(() => services.AddTestingDataDirectoryPathProvider());
+            return serviceAction;
         }
     }
 }
